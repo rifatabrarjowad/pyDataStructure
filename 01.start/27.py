dd3b4c98496fe631725e8929e5335eb274492def
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-11-03 23:15:57 +0600

    GitHub Copilot

---
+++ 01.start/27.py
@@ -0,0 +1,13 @@
+#binarry search
+def binary_search(array, target):
+    low = 0
+    high = len(array) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
---
+++ 01.start/app.js
@@ -0,0 +1,4 @@
+// find the lower case carekter charavter
+function findLowerCase(str) {
+    return str.match(/[a-z]/g);
+}
